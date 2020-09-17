ec7a7b485dc46cd4d8a17b8ac272d96f75b50c95
bjoernh2000 <bjoernh@bu.edu>
2020-09-17 14:18:29 +0200

    Initial commit

---
+++ precache-manifest.eb1476e8c44af138e40e10b694be4a4c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896e61cbc9254ebf2c377a5e2d8b7386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfeb1bb49109e969b5f0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8a084ac5423dc8822eb1",
+    "url": "/static/css/main.f66684a2.chunk.css"
+  },
+  {
+    "revision": "cfeb1bb49109e969b5f0",
+    "url": "/static/js/2.313c780a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.313c780a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a084ac5423dc8822eb1",
+    "url": "/static/js/main.d44967fd.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "e032198b4228526d1c243c093b7c3eac",
+    "url": "/static/media/DSC_0017.e032198b.JPG"
+  },
+  {
+    "revision": "1329956768fea6736fbe5c6dee91649e",
+    "url": "/static/media/mmexport1535890633461 2.13299567.JPG"
+  }
+]);
